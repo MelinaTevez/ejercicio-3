be584fcd1db6ce8ab14046f1dbef6539165ff26f
Melina Tevez <melinatevez15@gmail.com>
2022-06-02 15:50:25 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nombre
+nombre = prompt('Ingrese su nombre')
+console.log(nombre)
+
+
+let edad = prompt('Ingrese su edad')
+
+if (edad <=30) {
+    console.log('Hola, como andas?')
+} 
+else {
+    console.log('Bienvenido.Buenos días.')
+}
